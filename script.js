80fba57be855db3e59507fdfc13f2d02024b15d7
michalishadjiandreou1 <michalishadjiandreou1@penguin.lxd>
2024-06-13 21:26:49 +0100

    fixing page and js for the four tables - feature/3

--- script.js
+++ script.js
@@ -1,15 +1,29 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const groupAContainer = document.getElementById('group-a');
-    const groupBContainer = document.getElementById('group-b');
+    const bestteamContainer = document.getElementById('bestteam-table');
+    const worstteamContainer = document.getElementById('worstteam-table');
+    const mostredsContainer = document.getElementById('mostreds-table');
+    const topscorerContainer = document.getElementById('topscorer-table');
 
     // Simulated data for different groups, in real application you would fetch this data from an API
-    const groupA = [
+    const dataBestTeam = [
         { name: 'France', points: 15 },
         { name: 'Germany', points: 12 },
         { name: 'Italy', points: 10 }
     ];
 
-    const groupB = [
+    const dataWorstTeam = [
+        { name: 'Spain', points: 9 },
+        { name: 'Belgium', points: 7 },
+        { name: 'Portugal', points: 6 }
+    ];
+
+    const dataMostReds = [
+        { name: 'Spain', points: 9 },
+        { name: 'Belgium', points: 7 },
+        { name: 'Portugal', points: 6 }
+    ];
+
+    const dataTopScorer = [
         { name: 'Spain', points: 9 },
         { name: 'Belgium', points: 7 },
         { name: 'Portugal', points: 6 }
@@ -38,10 +52,15 @@ document.addEventListener('DOMContentLoaded', () => {
     // Simulate live ranking update
     setInterval(() => {
         // In a real application, you would fetch new data here
-        updateTeams(groupA, groupAContainer);
-        updateTeams(groupB, groupBContainer);
+        updateTeams(dataBestTeam, bestteamContainer);
+        updateTeams(dataWorstTeam, worstteamContainer);
+        updateTeams(dataMostReds, mostredsContainer);
+        updateTeams(dataTopScorerTeam, topscorerContainer);
     }, 10000); // Update every 10 seconds
 
-    updateTeams(groupA, groupAContainer); // Initial call to populate Group A
-    updateTeams(groupB, groupBContainer); // Initial call to populate Group B
+    updateTeams(dataBestTeam, bestteamContainer);  // Initial call to populate
+    updateTeams(dataWorstTeam, worstteamContainer);  // Initial call to populate
+    updateTeams(dataMostReds, mostredsContainer);  // Initial call to populate
+    updateTeams(dataTopScorer, topscorerContainer);  // Initial call to populate
+    
 });
