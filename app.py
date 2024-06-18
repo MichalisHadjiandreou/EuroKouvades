203a6ea6f4b8b4b6d0d852706470b915ac4f2d1d
michalishadjiandreou1 <michalishadjiandreou1@penguin.lxd>
2024-06-18 07:20:34 +0100

    changes on port config - feature/5

--- app.py
+++ app.py
@@ -197,4 +197,5 @@ if __name__ == '__main__':
         print("API key not found in environment variables.")
 
     #app.run(debug=True)
-    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
